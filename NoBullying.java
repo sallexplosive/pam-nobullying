c1ade103067f06d62e0d2ce53037194075dc4d87
SALOMAO VALENTIM DE OLIVEIRA DOS ANJOS <392950492@alunos.etec.itaqua>
2019-03-14 19:56:54 -0300

    Primeiro Passo

--- NoBullying.java
+++ NoBullying.java
@@ -2,5 +2,7 @@ public class NoBullying
 	{   public static void main(String[] args)
 		{     System.out.println('Programa NoBullying');   } 
 				System.out.println ("10 Ações para Acabar com a Pratica do Bullying");
+				System.out.println ("1° Passo: Verificar o Comportamento do Aluno/ Colega de Sala");
+				
 
 } 
